7b5bbe2c64dc8a6b33ae6c969a4a707efb508749
Tomasz <piotrowski.kat@gmail.com>
2019-01-12 18:47:28 +0100

    Step Add Inquiry processor

--- src/TaskManager.Web.Api/InquiryProcessing/ITaskByIdInquiryProcessor.cs
+++ src/TaskManager.Web.Api/InquiryProcessing/ITaskByIdInquiryProcessor.cs
@@ -1,9 +1,28 @@
 ﻿using System.Threading.Tasks;
+using TaskManager.Common.TypeMapping;
+using TaskManager.Data.QueryProcessors;
 
 namespace TaskManager.Web.Api.InquiryProcessing
 {
     public interface ITaskByIdInquiryProcessor
     {
         Task GetTaskById(long taskId);
+    }
+
+    class TaskByIdInquiryProcessor : ITaskByIdInquiryProcessor
+    {
+        private readonly IAutoMapper _autoMapper;
+        private readonly ITaskByIdQueryProcessor _queryProcessor;
+
+        public TaskByIdInquiryProcessor(IAutoMapper autoMapper, ITaskByIdQueryProcessor queryProcessor)
+        {
+            _autoMapper = autoMapper;
+            _queryProcessor = queryProcessor;
+        }
+
+        public Task GetTaskById(long taskId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
